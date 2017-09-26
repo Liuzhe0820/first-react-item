80b8f342a819fe6a8bf2be3d0469022c0166cd5d
liuzhe0820 <liu55zhe@163.com>
2017-09-26 09:38:35 +0800

    PC端头部已完成

--- src/js/root.js
+++ src/js/root.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router,Route,hashHistory} from 'react-router';
+import {Button} from 'antd';
+import PCIndex from './component/pcIndex.js';
+import 'antd/dist/antd.css';
 
 export default class Root extends React.Component{
   render(){
     return (
       //这里替换了之前的 Index，变成了程序的入口
-      <div>Init</div>
+      <div>
+      <PCIndex></PCIndex>
+      </div>
     );
   };
 };
 
-ReactDOM.render(<Root/>, document.getElementById('example'));
+ReactDOM.render(<Root/>, document.getElementById('mainContainer'));

f1a443e544a1cc1ce6212e0e594b4015ad94cc22
liuzhe0820 <liu55zhe@163.com>
2017-11-29 15:10:49 +0800

    PC端评论页面完成

---
+++ src/js/component/pcNewsDetails.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {Row, Col,BackTop} from 'antd';
+import PCHeader from './pcHeader';
+import PCFooter from './pcFooter';
+import PcNewsImageBlock from './pcNewsImageBlock';
+export default class PcNewsDetails extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            'newsItem': ''
+        };
+    };
+    //生命周期  页面加载完成
+    componentDidMount() {
+
+        var myFetchOptions = {
+            method: 'GET'
+        };
+        fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getnewsitem&uniquekey=" + this.props.params.uniquekey, myFetchOptions)
+            .then(response => response.json())
+            .then(json => {
+                this.setState({newsItem: json});
+                document.title = this.state.newsItem.title + '-React News|React驱动的新闻平台';
+            })
+    }
+    createMarkup() {
+        //创建标记
+        return {__html: this.state.newsItem.pagecontent}
+    };
+    //dangerouslySetInnerHTML  把原始的HTML加载到页面
+    render() {
+        return (
+            <div>
+                <PCHeader></PCHeader>
+                <Row>
+                    <Col span={2}></Col>
+                    <Col span={14} className='container'>
+                        <div className='articleContainer' dangerouslySetInnerHTML={this.createMarkup()}></div>
+                    </Col>
+                    <Col span={6}>
+                        <PcNewsImageBlock count={40} width='100%' cardTitle='相关新闻' type='top' imageWidth='142px'></PcNewsImageBlock>
+                    </Col>
+                    <Col span={2}></Col>
+                </Row>
+                <PCFooter></PCFooter>
+                <BackTop></BackTop>
+            </div>
+        );
+    };
+};
--- src/js/root.js
+++ src/js/root.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router,Route,hashHistory} from 'react-router';
-import {Button} from 'antd';
+import { Router, Route, hashHistory } from 'react-router';
+import { Button } from 'antd';
 import PCIndex from './component/pcIndex.js';
+import PcNewsDetails from './component/pcNewsDetails';
 import MobileIndex from './component/mobileIndex.js';
 import 'antd/dist/antd.css';
 import MediaQuery from 'react-responsive';
 
-export default class Root extends React.Component{
-  render(){
+export default class Root extends React.Component {
+  render () {
     return (
-      //这里替换了之前的 Index，变成了程序的入口
+      // 这里替换了之前的 Index，变成了程序的入口
       <div>
-      <MediaQuery query='(min-device-width: 1224px)'>
-        <PCIndex/>
-      </MediaQuery>
-      <MediaQuery query='(max-device-width: 1224px)'>
-        <MobileIndex/>
-      </MediaQuery>
+        <MediaQuery query='(min-device-width: 1224px)'>
+          <Router history={hashHistory}>
+            <Route path='/' component={PCIndex}></Route>
+            <Route path='/details/:uniquekey' component={PcNewsDetails}></Route>
+          </Router>
+        </MediaQuery>
+        <MediaQuery query='(max-device-width: 1224px)'>
+          <MobileIndex/>
+        </MediaQuery>
       </div>
-    );
-  };
-};
+    )
+  }
+}
 
-ReactDOM.render(<Root/>, document.getElementById('mainContainer'));
+ReactDOM.render(<Root/>, document.getElementById('mainContainer'))
